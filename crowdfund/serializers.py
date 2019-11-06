fe9744ebfd88fb7f87126646b17932313f880f06
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-11-06 01:36:36 +0100

    embedded updateserializer in Farm

--- crowdfund/serializers.py
+++ crowdfund/serializers.py
@@ -15,35 +15,43 @@ class CategorySerializer(ModelSerializer):
         # )
 
 
-
-class FarmSerializer(ModelSerializer):
+class UpdateSerializer(ModelSerializer):
 
     class Meta:
-        model = Farm
+        model = Update
         fields = (
-            '__all__'
+            "id", "activity", "slug", "description",
+            "report", "date", "created", "updated",
         )
 
         # read_only_fields = (
         #     'expires', 'user'
         # )
+
+
+class FarmSerializer(ModelSerializer):
+
+    updates = UpdateSerializer(source='update_set', many=True, read_only=True)
+
+    class Meta:
+        model = Farm
+        fields = (
+            "id", "name", "slug", "description", "status", "duration",
+            "location", "insurance_statement", "insured", "units",
+            "unit_in_stock", "price_per_unit_currency", "price_per_unit",
+            "roi", "image", "stage", "harvest_date", "start_date",
+            "end_date", "manger", "category", "updates", "created", "updated"
+        )
+
+        # read_only_fields = (
+        #     'expires', 'user'
+        # )
+
 
 class FarmManagerSerializer(ModelSerializer):
 
     class Meta:
         model = FarmManager
-        fields = (
-            '__all__'
-        )
-
-        # read_only_fields = (
-        #     'expires', 'user'
-        # )
-
-class UpdateSerializer(ModelSerializer):
-
-    class Meta:
-        model = Update
         fields = (
             '__all__'
         )
