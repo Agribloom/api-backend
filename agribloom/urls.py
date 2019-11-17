627bb4d818f3898be96b1cde9bac660279615f59
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-11-17 17:16:52 +0100

    restructure app urls

--- agribloom/urls.py
+++ agribloom/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('farm-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path("api/v1/auth/", include('accounts.urls')),
-    path("api/v1/", include('crowdfund.urls')),
-    path('api/v1/auth/', include('rest_auth.urls')),
-    path('api/v1/auth/registration/', include('rest_auth.registration.urls'))
+    path("v1/auth/", include('accounts.urls')),
+    path("v1/", include('crowdfund.urls')),
+    path('v1/auth/', include('rest_auth.urls')),
+    path('v1/auth/registration/', include('rest_auth.registration.urls'))
 ]
