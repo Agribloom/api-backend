0610425bc54cf2693fb823b50dfb578de305c326
Ochui Princewill <ochuiprincewill411@gmail.com>
2020-05-15 21:16:41 +0100

    Hooked transaction log to investment

--- crowdfund/apps.py
+++ crowdfund/apps.py
@@ -1,13 +1,15 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save
-import logging
+# import logging
 
 
 class CrowdfundConfig(AppConfig):
     name = 'crowdfund'
 
     def ready(self):
-        logging.info('34')
+        # logging.info('34')
+        from accounts.models import Transaction
         from crowdfund.models import Investment
-        from crowdfund.signals import update_farm_record
+        from crowdfund.signals import update_farm_record, create_investment_log
         post_save.connect(update_farm_record, sender=Investment)
+        post_save.connect(create_investment_log, sender=Investment)
--- crowdfund/signals.py
+++ crowdfund/signals.py
@@ -1,4 +1,4 @@
-from accounts.models import CustomUser
+from accounts.models import CustomUser, Transaction
 from crowdfund.models import Farm
 
 
@@ -14,3 +14,16 @@ def update_farm_record(sender, instance, created, **kwargs):
         if farm.unit_in_stock == 0:
             farm.status = Farm.PROJECT_SOLD_OUT
         farm.save()
+
+
+def create_investment_log(sender, instance, created, **kwargs):
+    if created:
+        transaction = Transaction.objects.create(
+            owner=instance.owner,
+            farm=instance.farm,
+            description='Paid',
+            amount=instance.amount,
+            status=Transaction.PAID,
+            transaction_type=Transaction.PURCHASE
+
+        )
